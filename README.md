26abb52eeeaf1b52cc04e56c9ee7b8bb13c06976
coddeee <chauhana089@gmail.com>
2022-07-20 13:45:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-sgey45
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-sgey45)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { productsFromFile } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = productsFromFile;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
